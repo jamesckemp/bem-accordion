68e8ddc8b04eb2da260d8fdaca01597399d2d9fe
James Kemp <me@jckemp.com>
2016-02-24 09:55:17 +0000

    Create example.php

---
+++ example.php
@@ -0,0 +1,31 @@
+<div class="accordion faqs" data-block="faqs">
+
+        <div class="faqs__item js-accordion--open">
+            <div class="icon--faq faqs__title js-accordion--open">
+                <h2>Question 1?</h2>
+            </div>
+            <div class="faqs__content js-accordion--open">
+                <div>
+                    <p>Answer 1 lorem ipsum dolor sit amet, consectetuer adipiscing elit. Donec odio.</p>
+                    <p>Quisque volutpat mattis eros. Nullam malesuada erat ut turpis. Suspendisse urna nibh, viverra non, semper suscipit, posuere a, pede.</p>
+                </div>
+            </div>
+        </div>
+        
+        <div class="faqs__item">
+            <div class="icon--faq faqs__title">
+                <h2>Question 2?</h2>
+            </div>
+            <div class="faqs__content">
+                <div>
+                    <p>Answer 2 lorem ipsum dolor sit amet, consectetuer adipiscing elit. Donec odio.</p>
+                    <p>Quisque volutpat mattis eros. Nullam malesuada erat ut turpis. Suspendisse urna nibh, viverra non, semper suscipit, posuere a, pede.</p>
+                </div>
+            </div>
+        </div>
+
+</div>
+
+<script>
+$('.accordion').accordion();
+</script>
